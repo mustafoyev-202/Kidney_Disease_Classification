3371482fcbd980897cd105c3b11dc48b10c5f5ce
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-10-17 00:57:02 +0500

    pipeline added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -10,24 +10,30 @@ class PredictionPipeline:
 
     def predict(self):
         # load model
-        model = load_model(os.path.join("artifacts", "training", "model.h5"))
+        model = load_model(os.path.join("model", "model.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
+
+        # Normalize the image
+        test_image = test_image / 255.0
         test_image = np.expand_dims(test_image, axis=0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+
+        # Get predictions
+        result = model.predict(test_image)
+        print("Raw prediction output:", result)
+
+        result = np.argmax(result, axis=1)
+        print("Predicted class index:", result[0])
 
         if result[0] == 0:
             prediction = 'Cyst'
-            return [{"image": prediction}]
         elif result[0] == 1:
             prediction = 'Normal'
-            return [{"image": prediction}]
         elif result[0] == 2:
             prediction = 'Stone'
-            return [{"image": prediction}]
         else:
             prediction = 'Tumor'
-            return [{"image": prediction}]
+
+        return [{"image": prediction}]
